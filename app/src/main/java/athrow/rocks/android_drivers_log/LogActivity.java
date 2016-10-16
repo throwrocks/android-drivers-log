b82a8a553ac71a5a579a04753e075e94236b3f9a
Jose <p0p.thr0w@gmail.com>
2016-10-15 22:20:45 -0500

    feat: Started implementing the LogActivity.

---
+++ app/src/main/java/athrow/rocks/android_drivers_log/LogActivity.java
@@ -0,0 +1,25 @@
+package athrow.rocks.android_drivers_log;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class LogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView fromSite = (AutoCompleteTextView) findViewById(R.id.from_site);
+        AutoCompleteTextView toSite = (AutoCompleteTextView) findViewById(R.id.to_site);
+        fromSite.setAdapter(adapter);
+        toSite.setAdapter(adapter);
+    }
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
+}
